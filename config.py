f8586fde4855ea3b2e0e524b4ebd0f7126f33ae7
Btmanjusms <90103484+Btmanjusms@users.noreply.github.com>
2023-09-02 23:26:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5833627856:AAFp3yWfj2ExBSAY7ko_0y-hIJe0AZ1655w")
     API_ID = int(os.environ.get("API_ID", 977080))
     API_HASH = os.environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
-    DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://abcd:abcd@cluster0.qirokuf.mongodb.net/?retryWrites=true&w=majority")
+    DB_URL = os.environ.get("DATABASE_URL", "postgres://pvsrfrxr:Mi26RmeIVN_8ox_RdHy5RLjkw4NCK7Bv@mouse.db.elephantsql.com/pvsrfrxr")
